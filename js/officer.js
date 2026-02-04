97541e29659d8878f0d6099cdd26a9fd8a79c29d
hope-mcmillen <hmcmil5@lsu.edu>
2026-02-04 15:53:12 -0600

    added ambassadors to officers

--- js/officer.js
+++ js/officer.js
@@ -1,22 +1,50 @@
-let preveiwContainer = document.querySelector('.officer-preview');
-let previewBox = preveiwContainer.querySelectorAll('.preview');
+let officerPreviewContainer = document.querySelector('.officer-preview');
+let ambassadorPreviewContainer = document.querySelector('.ambassador-preview');
+
+let ambassadorPreviewBox = ambassadorPreviewContainer.querySelectorAll('.preview');
+let officerPreviewBox = officerPreviewContainer.querySelectorAll('.preview');
+
 
 document.querySelectorAll('.officer-container .officer').forEach(officer =>{
   officer.onclick = () =>{
-    preveiwContainer.style.display = 'flex';
+    officerPreviewContainer.style.display = 'flex';
     let name = officer.getAttribute('data-name');
-    previewBox.forEach(preview =>{
-      let target = preview.getAttribute('data-target');
-      if(name == target){
-        preview.classList.add('active');
-      }
+    officerPreviewBox.forEach(preview =>{
+      preview.classList.toggle(
+        'active',
+        preview.getAttribute('data-target') == name
+      )
     });
   };
 });
 
-previewBox.forEach(close =>{
-  close.querySelector('.fa-times').onclick = () =>{
-    close.classList.remove('active');
-    preveiwContainer.style.display = 'none';
+document.querySelectorAll('.ambassador-container .ambassador').forEach(ambassador =>{
+  ambassador.onclick = () =>{
+    ambassadorPreviewContainer.style.display = 'flex';
+    let name = ambassador.getAttribute('data-name');
+    ambassadorPreviewBox.forEach(preview =>{
+      preview.classList.toggle(
+        'active',
+        preview.getAttribute('data-target') == name
+      )
+    });
   };
 });
+
+document.querySelectorAll('.preview .fa-times').forEach(closeBtn =>{
+  closeBtn.onclick = () =>{
+    let preview = closeBtn.parentElement;
+    let container = preview.parentElement
+
+    preview.classList.remove('active');
+    container.style.display = 'none';
+  };
+});
+
+/*previewBox.forEach(close =>{
+  close.querySelector('.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preview.parentElement.style.display = 'none';
+  };
+});
+*/
